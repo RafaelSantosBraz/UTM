d22be3a0bff8daa53625c0f1ab2799ae009bd19a
Rafael dos Santos Braz <rafinha.santos.braz@gmail.com>
2017-11-18 12:15:37 -0200

    início da implementação da UTM

---
+++ UTM/UTM/Posicao.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTM
+{
+    class Posicao
+    {
+        private char simbolo;
+        private Posicao ant;
+        private Posicao prox;
+
+        public Posicao()
+        {
+            Simbolo = '\0';
+            Ant = null;
+            Prox = null;
+        }
+
+        public char Simbolo { get => simbolo; set => simbolo = value; }
+        internal Posicao Ant { get => ant; set => ant = value; }
+        internal Posicao Prox { get => prox; set => prox = value; }
+    }
+}
